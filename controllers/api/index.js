f9bcdbb6806ac73ed7300f24d6eb6dbf3ada0d93
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-04-06 21:29:43 -0400

    games and comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 // TURN THESE ON LATER FOR COMMENTS AND POSTS WILL BE FOR THE GAMES 
-// const postRoutes = require('./post-routes');
+const gamesRoutes = require('./games-routes');
 // const commentsRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/games', gamesRoutes);
 // router.use('/comments', commentsRoutes);
 
 module.exports = router;
