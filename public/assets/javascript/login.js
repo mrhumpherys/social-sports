ae1089a18377b5ac11a92d62b5b5b55647d93bc9
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-04-10 15:18:06 -0400

    added checkbox functionality; made it to where it will keep you logged in for 24 hours

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -3,8 +3,29 @@ async function loginFormHandler(event) {
 
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    
+
+    if (document.querySelector('#remember-box').checked) {
+    const checkbox = document.querySelector('#remember-box:checked').value;
+
     if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password,
+                checkbox: checkbox ? checkbox : null
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        } 
+    }
+    } else {
+        if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
@@ -18,8 +39,13 @@ async function loginFormHandler(event) {
             document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
-        }
+        } 
     }
+
+    }
+
+
+    
 }
 // ===================  line 18 put replace dashboard =============================== //
 
