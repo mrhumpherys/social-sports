f1aacc18e50288daf7b64332ee8ed5cf0904a2b6
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-04-10 17:05:13 -0400

    added favicon; resized and adjusted pixels but is still not rendering; fixed issues with login and signup layout; commented out and removed the social media icons for now

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,52 +1,48 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-    if (document.querySelector('#remember-box').checked) {
-    const checkbox = document.querySelector('#remember-box:checked').value;
+  if (document.querySelector("#remember-box").checked) {
+    const checkbox = document.querySelector("#remember-box:checked").value;
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password,
-                checkbox: checkbox ? checkbox : null
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+      const response = await fetch("/api/users/login", {
+        method: "post",
+        body: JSON.stringify({
+          email,
+          password,
+          checkbox: checkbox ? checkbox : null,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        } 
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert(response.statusText);
+      }
     }
-    } else {
-        if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+  } else {
+    if (email && password) {
+      const response = await fetch("/api/users/login", {
+        method: "post",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        } 
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        alert(response.statusText);
+      }
     }
-
-    }
-
-
-    
+  }
 }
 // ===================  line 18 put replace dashboard =============================== //
 
-document.querySelector('#login').addEventListener('click', loginFormHandler);
+document.querySelector("#login").addEventListener("click", loginFormHandler);
