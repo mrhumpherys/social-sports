aabdd57dba546ec96d691aef6c9e1d4cde25bfd7
Jmax <jmax407@gmail.com>
2021-04-11 14:54:49 -0400

    preview text for news section

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -8,14 +8,30 @@
 //     });
 //   });
 
-let readMore = `<a href="{{Url}}"target="_blank" rel="noreopener">Read More <i class="fas fa-arrow-right"></i></a>`;
-let previewText = document.querySelectorAll('.news-article p');
-for (var i = 0; i < previewText.length; i++) {
-    let previewTextContent = previewText[i].innerText;
-    // console.log(previewTextContent);
-    let updatedpreviewTextContent = previewTextContent.slice(0, 140);
-    //previewText = updatedpreviewTextContent;
-    console.log(updatedpreviewTextContent);
-    //previewText[i] = updatedpreviewTextContent;
-}
+// let readMore = `<a href="{{Url}}"target="_blank" rel="noreopener">Read More <i class="fas fa-arrow-right"></i></a>`;
+// let previewText = document.querySelectorAll('.news-article p');
+// for (var i = 0; i < previewText.length; i++) {
+//     let previewTextContent = previewText[i].innerText;
+//     // console.log(previewTextContent);
+//     let updatedpreviewTextContent = previewTextContent.slice(0, 140);
+//     //previewText = updatedpreviewTextContent;
+//     console.log(updatedpreviewTextContent);
+//     //previewText[i] = updatedpreviewTextContent;
+// }
 
+let gameCardHeight = document.querySelectorAll('.game-card')[0].offsetHeight;
+let newsCardEl = document.querySelector('.news-container');
+// let heightCheck = function () {
+//     newsCardEl.style.height(gameCardHeight);
+// }
+// heightCheck();
+// console.log(gameCardHeight);
+
+// $(function() {
+//     $(newsCardEl).css("height", gameCardHeight);
+//   });
+
+
+//   window.onresize = function() {
+//     $(newsCardEl).css("height", gameCardHeight);
+//   }
